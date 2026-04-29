b2de8ec73a2b8506688d306503831041232b442d
smyy7864-arch <smyy7864@gmail.com>
2026-04-29 23:08:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const axios = require('axios'); // שים לב שכתוב axios
+const axios = require('axios');
 const app = express();
 
 app.set('trust proxy', true);
